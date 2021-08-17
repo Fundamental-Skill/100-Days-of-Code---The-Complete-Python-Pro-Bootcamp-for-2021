fce10e2c8b185c7f00ca256a312839810ba33d20
ade.ristanto@gmail.com <56842597-Ade>
2021-08-17 09:20:39 +0700

    Final Day 1

---
+++ Day 1 - Beginner - Working with Variables in Pyhon to Manage Data/band-name-generator-start/main.py
@@ -0,0 +1,11 @@
+# 1. Create a greeting for your program.
+print("Band Name Generator".upper())
+
+# 2. Ask the user for the city that they grew up in.
+city = input("City your grew up? ")
+# 3. Ask the user for the name of a pet.
+pet = input("Your pet name? ")
+# 4. Combine the name of their city and pet and show them their band name.
+print("Your band name: ", city + " " + pet)
+# 5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
---
+++ Day 1 - Beginner - Working with Variables in Pyhon to Manage Data/day-1-4-exercise/main.py
@@ -0,0 +1,12 @@
+a = 3
+b = 5
+
+print(f"a: {a}")
+print(f"b: {b}")
+
+c = a
+a = b
+b = c
+
+print(f"a: {a}")
+print(f"b: {b}")
