53ed9326fa1f7dbc2697314a476f894984981857
ade.ristanto@gmail.com <56842597-Ade>
2021-08-24 16:42:35 +0700

    4. Randomisation

---
+++ Day 4 - Beginner - Randomisation and Python Lists/day-4-1-exercise/main.py
@@ -0,0 +1,10 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+import random
+
+rn = random.randint(0, 1)
+
+if rn == 0:
+    print(f"{rn} means Head")
+else:
+    print(f"{rn} means Tails")
